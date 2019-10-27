0ad126430ab388f5a845f6e8853ef59810bc8aab
m19 <m19@mint-tortosa20>
2019-10-27 15:12:04 +0100

    Versió 0.6-C del projecte prjava43cognom20. Branca00 modificada.

--- src/prjava43tortosac20/prjava43tortosa20.java
+++ src/prjava43tortosac20/prjava43tortosa20.java
@@ -1,6 +1,8 @@
 package prjava43tortosac20;
 import java.awt.*; // using AWT containers and components
 import java.awt.event.*; // using AWT events and listener interfaces
+import java.text.*;
+import java.io.*;
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class prjava43tortosa20 extends Frame
 implements ActionListener, WindowListener {
